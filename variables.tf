15b5e4c20d4b4cbed8f229a1a71fe553afe5fd1c
ravicheetiralatl <126625288+ravicheetiralatl@users.noreply.github.com>
2023-09-22 09:15:12 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "prefix" {
   description = "Name for the RDS instance"
   default     = "default" # Provide a default value if needed
 }
+
+variable "enginetype" {
+  description = "Type of the RDS instance"
+  default     = "postgres" # Provide a default value if needed
+}
+
+variable "engineversion" {
+  description = "Type of the RDS instance"
+  default     = "15.4" # Provide a default value if needed
+}
