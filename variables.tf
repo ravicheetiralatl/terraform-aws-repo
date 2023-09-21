df9429a2f1a385afb8318c5da4a3d9da2957d2c2
ravicheetiralatl <126625288+ravicheetiralatl@users.noreply.github.com>
2023-09-21 13:43:17 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+# Define a variable to hold the instance name
+variable "instance_name" {
+  description = "Name for the RDS instance"
+  default     = "default-name" # Provide a default value if needed
+}
