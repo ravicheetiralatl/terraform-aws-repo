1669121772cee28e1312f27ad01bc630c576469d
ravicheetiralatl <126625288+ravicheetiralatl@users.noreply.github.com>
2023-09-22 09:53:24 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,8 +29,8 @@ provider "aws" {
 resource "aws_db_instance" "rds" {
   allocated_storage    = 20
   storage_type         = "gp2"
-  engine               = var.enginetype
-  engine_version       = var.engineversion
+  engine               = ${var.enginetype}
+  engine_version       = ${var.engineversion}
   instance_class       = "db.t4g.medium"
   identifier           = "rds${var.prefix}"
   db_name              = "db${var.prefix}"
